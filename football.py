d6d127a4e0eeb5c77fb8fd654458974629c10bbd
Andrew Clarke <andrewneilclarke@gmail.com>
2021-03-14 17:00:53 +0100

    added teams form data

--- football.py
+++ football.py
@@ -12,6 +12,25 @@ file = 'teams_data.json'
 with open(file, 'w') as f:
   json.dump(teams_data, f)
 
+file = 'form_guide.json'
+with open(file, 'r') as f:
+  form_data = json.load(f)
+
+teams_form_data = form_data['form-guide']['teams']
+file = 'teams_form_data.json'
+with open(file, 'w') as f:
+  json.dump(teams_data, f)
+
+file = 'fixtures_data.json'
+with open(file, 'r') as f:
+  fixtures_data = json.load(f)
+teams_fixtures_data = fixtures_data['fixtures-results']['team']
+
+file = 'predictions_data.json'
+with open(file, 'r') as f:
+  predictions_data = json.load(f)
+predictions = predictions_data['api']['predictions']
+
 class Team:
     def __init__(self, position, total_points, name):
         self.name = name
@@ -35,7 +54,8 @@ class Team:
         self.awaydrawn = teams_data[self.position -1]['away-matches']['drawn']
         self.awaylost = teams_data[self.position -1]['away-matches']['lost']
         self.awayfor = teams_data[self.position -1]['away-matches']['for']
-        self.awayagainst = teams_data[self.position -1]['away-matches']['against']        
+        self.awayagainst = teams_data[self.position -1]['away-matches']['against']
+        self.form = teams_form_data[self.position -1]['points'] / teams_form_data[self.position -1]['played']       
 
     def get_descriptive_name(self):
         """Return a neatly formatted team description """
@@ -44,12 +64,12 @@ class Team:
     @property
     def position_rating(self):
         return 20 - self.position + 1
-    """
+    
     @property
     #marks out of 20
     def form_rating(self):
-        return round(self.pointsinlastten * 0.666666)
-    """
+        return round(self.form * 8)
+    
     @property
     #marks out of 10
     def goal_threat_rating(self):
@@ -67,13 +87,13 @@ class Team:
     @property
     def away_rating(self):
         return round((self.awaywon) * 3) + (self.awaydrawn)  / (self.awayplayed) * 6
-  
-    def calculate_home_team(self):
+    @property
+    def home_team(self):
         return str(int(round((self.position_rating + self.form_rating + self.goal_threat_rating + self.defence_rating + self.home_rating), -1)/10))
-
-    def calculate_away_team(self):
+    @property
+    def away_team(self):
         return str(int(round(self.position_rating + self.form_rating + self.goal_threat_rating + self.defence_rating + self.away_rating, -1)/10))
-          
+
 
 #create team instances
 teams = [] 
@@ -81,19 +101,19 @@ for team in teams_data:
     team = Team(team['position'], team['total-points'], team['name'])
     teams.append(team)
 
-# #print table
+def print_table():
+    print('Team Name   P  W  D  L  F  A  GD  FORM')
+    for team in teams:
+        print(team.name, team.played, team.won, team.drawn, team.lost, team.gf, 
+                team.ga, team.gd, team.form_rating, team.home_team)
+
 # for team in teams:
-#     print(team.name, team.played, team.won, team.drawn, team.lost, team.ga, team.gf, team.gd)
+#   print(team.name + team.home_team())
 
-# # print defence ratings
-# for team in teams:
-#     print(team.defence_rating)
 
-# print defence ratings
-for team in teams:
-    print(team.away_rating)
+#print_table()
+print(predictions[0]['advice'])
 
-# print('Burnley: ' + Burnley.calculate_home_team())
 # print('Arsenal: ' + Arsenal.calculate_away_team() + '\n')
 # print('SheffU: ' + SheffU.calculate_home_team())
 # print('Southampton: ' + Southampton.calculate_away_team() + '\n')
--- get_api_data.py
+++ get_api_data.py
@@ -12,6 +12,7 @@ api_key = keys['api_key']
 # load urls
 table_url = "https://football-web-pages1.p.rapidapi.com/league-table.json"
 form_url = "https://football-web-pages1.p.rapidapi.com/form-guide.json"
+fixtures_url = "https://football-web-pages1.p.rapidapi.com/fixtures-results.json"
 querystring = {"comp":"1","team":"1"}
 headers = {
     'x-rapidapi-key': api_key,
@@ -20,6 +21,17 @@ headers = {
 # make get requests
 r = requests.request("GET", table_url, headers=headers, params=querystring)
 r2 = requests.request("GET", form_url, headers=headers, params=querystring)
+querystring = {"comp":"1","team":"1","round":"1"}
+r3 = requests.request("GET", fixtures_url, headers=headers, params=querystring)
+
+# get predictions
+predictions_url = "https://api-football-v1.p.rapidapi.com/v2/predictions/157462"
+headers = {
+    'x-rapidapi-key': "ec77ba6a5fmsh79e0926558f3a80p19b4aejsn907505bcb806",
+    'x-rapidapi-host': "api-football-v1.p.rapidapi.com"
+    }
+r4 = requests.request("GET", predictions_url, headers=headers)
+
 #store responses
 file = 'league_table.json'
 with open(file, 'w') as f:
@@ -28,3 +40,11 @@ with open(file, 'w') as f:
 file = 'form_guide.json'
 with open(file, 'w') as f:
 	json.dump(r2.json(), f, indent=4,)
+
+file = 'fixtures_data.json'
+with open(file, 'w') as f:
+	json.dump(r3.json(), f, indent=4,)
+
+file = 'predictions_data.json'
+with open(file, 'w') as f:
+	json.dump(r4.json(), f, indent=4,)
